7db91e5c2de304fa94b25b3b33842dff31774f17
aadridi <aadridi@gmail.com>
2023-08-11 20:06:09 +0200

    Coursera JS "hard" assignment

--- module4-solution/harder/script.js
+++ module4-solution/harder/script.js
@@ -34,7 +34,8 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
@@ -56,9 +57,10 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
+    if (/* fill in condition here */) {
     // byeSpeaker.xxxx
-  } else {
+    }
+    else {
     // helloSpeaker.xxxx
   }
-}
+})();
